9eeb9e3803e8803fe2499531df68823e1f4a15fa
Lee Page <leep@Lees-MacBook-Pro.local>
2015-10-08 16:21:32 -0500

    Works with string interpolation.

--- main.rb
+++ main.rb
@@ -1,5 +1,6 @@
+require_relative 'DifferentCurrencyCodeError'
 require_relative "currency"
-
+begin
 john = Currency.new("USD", 1)
 paul = Currency.new("JPY", 4)
 george = Currency.new("EUR", 23)
@@ -11,12 +12,16 @@ puts "John has #{john}", "Paul has #{paul}"
 puts "George has #{george}", "Ringo has #{ringo}"
 puts "Eddie has #{eddie}", "Alex has #{alex}"
 puts ""
-puts "John = Paul? #{john.equals(paul)}"
-puts "John = Eddie? #{john.equals(eddie)}"
-puts "Eddie = Alex? #{eddie.equals(alex)}"
-puts "John + Eddie = #{john.plus(eddie)}"
-puts "Eddie + Alex = #{eddie.plus(alex)}"
-puts "Ringo + Alex = #{ringo.plus(alex)}"
-puts "John - Eddie = #{john.minus(eddie)}"
-puts "Eddie - Alex = #{eddie.minus(alex)}"
-puts "Ringo - Alex = #{ringo.minus(alex)}"
+
+puts "John = Paul? #{john.===(paul)}"
+puts "John = Eddie? #{john.===(eddie)}"
+puts "Eddie = Alex? #{eddie.===(alex)}"
+puts "John + Eddie = #{john.+(eddie)}"
+puts "Eddie + Alex = #{eddie.+(alex)}"
+puts "Ringo + Alex = #{ringo.+(alex)}"
+puts "John - Eddie = #{john.-(eddie)}"
+puts "Eddie - Alex = #{eddie.-(alex)}"
+puts "Ringo - Alex = #{ringo.-(alex)}"
+rescue
+	puts "You can't do that shit."
+end
