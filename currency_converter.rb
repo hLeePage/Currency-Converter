46647f8e7b18dfc6c09ecb3f0fdc3b4d07fd6bdb
Lee Page <leep@Lees-MBP.local.tld>
2015-10-11 17:54:56 -0500

    Uncomment DifferentCurrencyCodeError

--- currency_converter.rb
+++ currency_converter.rb
@@ -1,5 +1,6 @@
 require_relative 'DifferentCurrencyCodeError'
 require_relative 'currency'
+
 class CurrencyConverter
 	def initialize hash
 		@rates = hash
@@ -7,7 +8,9 @@ class CurrencyConverter
 
 	def convert(currency, target_code)
 		rate = @rates[currency.code][target_code]
-		puts "The rate is #{rate}"
+		puts Currency.new(target_code, currency.amount * rate)
 	end
 
 end
+
+
