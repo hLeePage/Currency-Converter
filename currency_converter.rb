c98982ddd488c4db8483c05e689613fbee593e62
Lee Page <hLeePage@users.noreply.github.com>
2015-10-12 09:20:43 -0500

    Update currency_converter.rb

--- currency_converter.rb
+++ currency_converter.rb
@@ -3,12 +3,12 @@ require_relative 'currency'
 
 class CurrencyConverter
   def initialize hash
-  @rates = hash
+    @rates = hash
   end
 
   def convert(currency, target_code)
-  rate = @rates[currency.code][target_code]
-  puts Currency.new(target_code, currency.amount * rate)
+    rate = @rates[currency.code][target_code]
+    puts Currency.new(target_code, currency.amount * rate)
   end
 
 end
