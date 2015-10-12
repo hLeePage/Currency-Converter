3642c683f340ebdaef3b1bc932a23a551309504c
Lee Page <hLeePage@users.noreply.github.com>
2015-10-12 09:13:44 -0500

    Update currency_converter.rb

--- currency_converter.rb
+++ currency_converter.rb
@@ -2,14 +2,14 @@ require_relative 'DifferentCurrencyCodeError'
 require_relative 'currency'
 
 class CurrencyConverter
-	def initialize hash
-		@rates = hash
-	end
+  def initialize hash
+  @rates = hash
+  end
 
-	def convert(currency, target_code)
-		rate = @rates[currency.code][target_code]
-		puts Currency.new(target_code, currency.amount * rate)
-	end
+  def convert(currency, target_code)
+  rate = @rates[currency.code][target_code]
+  puts Currency.new(target_code, currency.amount * rate)
+  end
 
 end
 
